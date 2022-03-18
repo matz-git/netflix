e05d35f7a0c5a35f7c0d011122d8e2ec94a58551
matz-git <s.steil2@gmail.com>
2022-03-18 19:08:44 +0100

    add signin/signup page and protected routes with firebaseAuth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages'; 
 import * as ROUTES from './constants/routes';
+// import {IsUserRedirect, ProtectedRoute } from './helpers/routes';
+// import { useAuthListener } from './hooks';
 
 export default function App() {
-  return (
+
+    const user = null;
+    
+
+    function RequireAuth({ children, redirectTo, user }) {
+        // let isAuthenticated = getAuth();
+        return user ? children : <Navigate to={redirectTo} />;
+    }
+
+    return (
+        // OLD:
+        // <Router>
+        //     <Routes>
+        //         <Route exact path="/browse" element={<Browse />} />
+        //         <Route exact path="/signin" element={<Signin />} />
+        //         <Route exact path="/signup" element={<Signup />} />
+        //         <Route exact path={ROUTES.HOME} element={<Home />} />
+        //     </Routes>
+        // </Router>
+
         <Router>
             <Routes>
-                <Route exact path="/browse" element={<Browse />} />
-                <Route exact path="/signin" element={<Signin />} />
-                <Route exact path="/signup" element={<Signup />} />
-                <Route exact path={ROUTES.HOME} element={<Home />} />
+
+                <Route path={ROUTES.HOME} element={<Home />} />
+                <Route 
+                    path={ROUTES.SIGN_IN} 
+                    element={user ? <Browse/> : <Signin />}
+                />
+                <Route 
+                    path={ROUTES.SIGN_UP} 
+                    element={user ? <Browse/> : <Signup />}
+                />
+                <Route 
+                    path={ROUTES.BROWSE} 
+                    element={
+                        <RequireAuth redirectTo={ROUTES.SIGN_IN} user={user}>
+                            <Browse />
+                        </RequireAuth>
+                    }
+                />
+
             </Routes>
         </Router>
     );
