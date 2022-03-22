b4aa84b0d0a2f0d7ec5fb0f577185e0874637f97
matz-git <s.steil2@gmail.com>
2022-03-22 12:10:21 +0100

    create browse page

---
+++ src/hooks/use-content.js
@@ -0,0 +1,32 @@
+import { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../context/firebase';
+
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+
+    let isMounted = true;
+
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const allContent = snapshot.docs.map((contentObj) => ({
+          ...contentObj.data(),
+          docId: contentObj.id,
+        }));
+
+        if (isMounted)
+         setContent(allContent);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    return () => { isMounted = false };
+  }, []);
+
+  return { [target]: content };
+}
